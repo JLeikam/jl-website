a4e917a86eb6212836cc96850b585c1feabdc3f0
Josh <j.leikam7@gmail.com>
2020-05-22 13:52:28 -0500

    about edits

--- js/typewriter.js
+++ js/typewriter.js
@@ -2,6 +2,6 @@ new TypeIt(".typewriter-text", {
   strings: ["Software", "Self", "Meta"],
   waitUntilVisible: true,
   breakLines: false,
-  speed: 150,
-  loop: true,
+  speed: 100,
+  loop: true
 }).go();
