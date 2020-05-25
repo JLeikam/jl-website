3ee634e0e5ca14455ea52176b68d3f8f7e7abb89
Josh <j.leikam7@gmail.com>
2020-05-25 12:01:13 -0500

    fix footer

--- js/particles.js
+++ js/particles.js
@@ -1,3 +1,3 @@
-particlesJS.load("particles-js", "js/particles.json", function () {
+particlesJS.load("particles-js", "js/particlesjs-config.json", function () {
   console.log("callback - particles.js config loaded");
 });
